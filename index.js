5e30eb5e83ca1927c99ee4f75759c414aed2211e
mahde13 <musavemahdy>
2024-06-14 21:19:40 -0800

    starting chat project and install express ,cors, JWt ,mongoose , cookeParser and morgan

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+dotenv.config();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(
+  cors({
+    origin: "*",
+    method: ["GET", "PATCH", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(cookieParser());
+// app.use(morgan("dev"));
+
+app.get("/", async (req, res) => {
+  return res.json({
+    data: "hello mahdy",
+  });
+});
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
