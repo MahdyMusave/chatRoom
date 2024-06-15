fae88ebd4a2cedcd97333258e7a2bcbd77a39498
mahde13 <musavemahdy>
2024-06-15 09:38:18 -0800

    client

--- sever/router/authRouter.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const { register, login, logout } = require("../controller/authCtrl");
-
-router.post("/register", register);
-router.post("/login", login);
-router.get("/logout", logout);
-
-module.exports = router;
